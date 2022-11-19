ec84169c5daebbc237cbc847635a36c70e534152
Luis Eduardo <lepereira47@gmail.com>
2022-11-19 01:07:00 -0300

    Criando provider teste

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
 import NextAuth, { NextAuthOptions } from "next-auth";
 
+import CredentialsProvider from "next-auth/providers/credentials";
+
 export const authOptions: NextAuthOptions = {
   secret: process.env.NEXTAUTH_SECRET,
   providers: [],
