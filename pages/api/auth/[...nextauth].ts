c2c5f4dd7a2cc507d8e28a4066b049540b2cbc29
Luis Eduardo <lepereira47@gmail.com>
2022-11-19 04:10:54 -0300

    novas function adicionadas

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -4,7 +4,27 @@ import CredentialsProvider from "next-auth/providers/credentials";
 
 export const authOptions: NextAuthOptions = {
   secret: process.env.NEXTAUTH_SECRET,
-  providers: [],
+  providers: [
+    CredentialsProvider({
+      id: "credentials",
+      credentials: {
+        email: { label: "E-mail", type: "text" },
+        senha: { label: "Senha", type: "password" },
+      },
+      authorize: async (credentials, req) => {
+        if (credentials?.email === "luis@teste.com") {
+          const user = {
+            id: "123",
+            name: "Luis",
+            email: "luis@teste.com",
+            role: "USER",
+          };
+          return user;
+        }
+        return null;
+      },
+    }),
+  ],
 };
 
 export default NextAuth(authOptions);
