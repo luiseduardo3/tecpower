a670de81c3a0f037984f9fce6b0e3247f2d0a399
Luis Eduardo <lepereira47@gmail.com>
2022-11-19 00:57:58 -0300

    Next Auth Login

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,8 @@
+import NextAuth, { NextAuthOptions } from "next-auth";
+
+export const authOptions: NextAuthOptions = {
+  secret: process.env.NEXTAUTH_SECRET,
+  providers: [],
+};
+
+export default NextAuth(authOptions);
